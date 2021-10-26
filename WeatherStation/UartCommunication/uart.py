887ea84161807ab57abc5fbc6499ecedc351a45b
VilleJes <villejestila@gmail.com>
2021-10-26 12:25:09 +0300

    fixes

--- WeatherStation/UartCommunication/uart.py
+++ WeatherStation/UartCommunication/uart.py
@@ -1,14 +1,14 @@
-import serial
 import threading
-import datetime, time
-import sqlite3
-import atexit
 
 import weather_station_1
 
+w_station_1 = weather_station_1.Weather_Station("w_station_1", 
+												'/dev/ttyACM0',
+												9600,
+												2,
+												'/home/pi/shared/weatherDBtest.sqlite3')
 
-w_station_1 = weather_station_1.Weather_Station("w_station_1", serial.Serial(port='/dev/ttyACM0',baudrate=9600),
-												 sqlite3.connect('/home/pi/shared/weatherDBtest.sqlite3'))
+
 
 class Thread(threading.Thread):
 	def __init__(self, t, *args):
@@ -17,7 +17,7 @@ class Thread(threading.Thread):
 
 
 def main():
-	weather_station = Thread(w_station_1.serial_data())
+	weather_station = Thread(w_station_1.serial_data)
 
 
 if __name__ == "__main__":
--- WeatherStation/UartCommunication/weather_station_1.py
+++ WeatherStation/UartCommunication/weather_station_1.py
@@ -1,25 +1,25 @@
 import serial
-import datetime, time
+import datetime
 import sqlite3
-import atexit
-#import threading
+from time import sleep
+import threading
+
 
 class Weather_Station:
 
-	def __init__(self, station_name, ser, con):
+	def __init__(self, station_name, port, baudrate, timeout, db_name):
 		self.station_name = station_name
-		self.ser = ser
-		self.con = con
-
+		self.port = port
+		self.baudrate = baudrate
+		self.timeout = timeout
+		self.con = sqlite3.connect(db_name, check_same_thread=False)
+		self.ser = serial.Serial(port=self.port, baudrate=self.baudrate, timeout=self.timeout)
 
 	def serial_data(self):
 		lux = 0
 		temp = 0
 		humidity = 0
 
-		#ser = serial.Serial(port='/dev/ttyACM0',baudrate=9600,timeout=2)
-
-		#con = sqlite3.connect('/home/pi/shared/weatherDBtest.sqlite3')
 		c = self.con.cursor()
 
 		while True:
@@ -38,7 +38,7 @@ class Weather_Station:
 				print(lux)
 				print(temp)
 				print(humidity)
-				#print(threading.current_thread())
+				print(threading.current_thread())
 				#print(date)
 				#print("\n")
 
@@ -46,3 +46,4 @@ class Weather_Station:
 				c.execute(f"INSERT INTO weather(date, sensorName, lux, temp, humidity) VALUES (?, ?, ?, ?, ?)", 
 																(date, self.station_name, lux, temp, humidity))
 				self.con.commit()
+				sleep(1)
